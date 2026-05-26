fb7b5451294719ff3750dcb14cfe3049351bc1ea
2Erik <erik19yogo@gmail.com>
2026-05-26 06:10:06 -0500

    ERT: Creacion tabla de rompimiento

--- biblioteca_db.sql
+++ biblioteca_db.sql
@@ -9,3 +9,17 @@ CREATE TABLE autores(
     nombre VARCHAR(50) NOT NULL,
     pais VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE libro_autor(
+    la_libro_codigo_fk VARCHAR(5) NOT NULL,
+    la_autor_id_fk INT NOT NULL,
+    anio_publicacion INT NOT NULL,
+
+    PRIMARY KEY(la_libro_codigo_fk, la_autor_id_fk),
+
+    FOREIGN KEY(la_libro_codigo_fk)
+        REFERENCES libros(codigo),
+
+    FOREIGN KEY(la_autor_id_fk)
+        REFERENCES autores(id)
+);
